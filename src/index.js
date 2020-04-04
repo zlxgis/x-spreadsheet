51b6871e520afa35eece9d121aaa87785c2a3919
zlxgis <zlxgis@163.com>
2020-04-04 19:27:09 +0800

    add

--- src/index.js
+++ src/index.js
@@ -9,100 +9,104 @@ import './index.less';
 
 
 class Spreadsheet {
-  constructor(selectors, options = {}) {
-    let targetEl = selectors;
-    this.options = options;
-    this.sheetIndex = 1;
-    this.datas = [];
-    if (typeof selectors === 'string') {
-      targetEl = document.querySelector(selectors);
+    constructor(selectors, options = {}) {
+        let targetEl = selectors;
+        this.options = options;
+        this.sheetIndex = 1;
+        this.datas = [];
+        if (typeof selectors === 'string') {
+            targetEl = document.querySelector(selectors);
+        }
+        this.bottombar = new Bottombar(() => {
+            const d = this.addSheet();
+            this.sheet.resetData(d);
+        }, (index) => {
+            const d = this.datas[index];
+            this.sheet.resetData(d);
+        }, () => {
+            this.deleteSheet();
+        }, (index, value) => {
+            this.datas[index].name = value;
+        });
+        this.data = this.addSheet();
+        const rootEl = h('div', `${cssPrefix}`)
+            .on('contextmenu', evt => evt.preventDefault());
+        // create canvas element
+        targetEl.appendChild(rootEl.el);
+        this.sheet = new Sheet(rootEl, this.data);
+        rootEl.child(this.bottombar.el);
     }
-    this.bottombar = new Bottombar(() => {
-      const d = this.addSheet();
-      this.sheet.resetData(d);
-    }, (index) => {
-      const d = this.datas[index];
-      this.sheet.resetData(d);
-    }, () => {
-      this.deleteSheet();
-    }, (index, value) => {
-      this.datas[index].name = value;
-    });
-    this.data = this.addSheet();
-    const rootEl = h('div', `${cssPrefix}`)
-      .on('contextmenu', evt => evt.preventDefault());
-    // create canvas element
-    targetEl.appendChild(rootEl.el);
-    this.sheet = new Sheet(rootEl, this.data);
-    rootEl.child(this.bottombar.el);
-  }
 
-  addSheet(name, active = true) {
-    const n = name || `sheet${this.sheetIndex}`;
-    const d = new DataProxy(n, this.options);
-    d.change = (...args) => {
-      this.sheet.trigger('change', ...args);
-    };
-    this.datas.push(d);
-    // console.log('d:', n, d, this.datas);
-    this.bottombar.addItem(n, active);
-    this.sheetIndex += 1;
-    return d;
-  }
-
-  deleteSheet() {
-    const [oldIndex, nindex] = this.bottombar.deleteItem();
-    if (oldIndex >= 0) {
-      this.datas.splice(oldIndex, 1);
-      if (nindex >= 0) this.sheet.resetData(this.datas[nindex]);
+    addSheet(name, active = true) {
+        const n = name || `sheet${this.sheetIndex}`;
+        const d = new DataProxy(n, this.options);
+        d.change = (...args) => {
+            this.sheet.trigger('change', ...args);
+        };
+        this.datas.push(d);
+        // console.log('d:', n, d, this.datas);
+        this.bottombar.addItem(n, active);
+        this.sheetIndex += 1;
+        return d;
     }
-  }
 
-  loadData(data) {
-    // const d = Array.isArray(data) ? data[0] : data;
-    const ds = Array.isArray(data) ? data : [data];
-    for (let i = 1; i < ds.length; i += 1) {
-      const it = ds[i];
-      const nd = this.addSheet(it.name, false);
-      nd.setData(it);
+    deleteSheet() {
+        const [oldIndex, nindex] = this.bottombar.deleteItem();
+        if (oldIndex >= 0) {
+            this.datas.splice(oldIndex, 1);
+            if (nindex >= 0) this.sheet.resetData(this.datas[nindex]);
+        }
     }
-    this.bottombar.renameItem(0, ds[0].name);
-    this.sheet.loadData(ds[0]);
-    return this;
-  }
 
-  getData() {
-    return this.datas.map(it => it.getData());
-  }
+    loadData(data) {
+        // const d = Array.isArray(data) ? data[0] : data;
+        const ds = Array.isArray(data) ? data : [data];
+        for (let i = 1; i < ds.length; i += 1) {
+            const it = ds[i];
+            const nd = this.addSheet(it.name, false);
+            nd.setData(it);
+        }
+        this.bottombar.renameItem(0, ds[0].name);
+        this.sheet.loadData(ds[0]);
+        return this;
+    }
 
-  on(eventName, func) {
-    this.sheet.on(eventName, func);
-    return this;
-  }
+    getData() {
+        return this.datas.map(it => it.getData());
+    }
 
-  validate() {
-    const { validations } = this.data;
-    return validations.errors.size <= 0;
-  }
+    on(eventName, func) {
+        this.sheet.on(eventName, func);
+        return this;
+    }
 
-  change(cb) {
-    this.sheet.on('change', cb);
-    return this;
-  }
+    validate() {
+        const { validations } = this.data;
+        return validations.errors.size <= 0;
+    }
 
-  static locale(lang, message) {
-    locale(lang, message);
-  }
+    change(cb) {
+        this.sheet.on('change', cb);
+        return this;
+    }
+    exports(fname) {
+
+    }
+
+
+    static locale(lang, message) {
+        locale(lang, message);
+    }
 }
 
 const spreadsheet = (el, options = {}) => new Spreadsheet(el, options);
 
 if (window) {
-  window.x_spreadsheet = spreadsheet;
-  window.x_spreadsheet.locale = (lang, message) => locale(lang, message);
+    window.x_spreadsheet = spreadsheet;
+    window.x_spreadsheet.locale = (lang, message) => locale(lang, message);
 }
 
 export default Spreadsheet;
 export {
-  spreadsheet,
+    spreadsheet,
 };
